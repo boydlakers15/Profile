2a67b8938e120d1e853dd0706d8b0a4940906e02
Jared Boyd <jboyd115@icloud.com>
2024-01-05 14:48:38 +0100

    New

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css'; // Import the Menu styles
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+    const handleEnterButtonClick = () => {
+
+        navigate("/Home");
+    };
 
   return (
     <nav>
@@ -9,7 +15,7 @@ const Navbar = () => {
         <li className={activeSection === 'home' ? 'active' : ''}><a href="/Profile/Home">Home</a></li>
         <li className={activeSection === 'profile' ? 'active' : ''}><a href="/Profile">Profile</a></li>
         <li className={activeSection === 'about' ? 'active' : ''}><a href="#about">About Me</a></li>
-        <li className={activeSection === 'skills' ? 'active' : ''}><a href="/Skills">Skills</a></li>
+        <li className={activeSection === 'skills' ? 'active' : ''}><a href=""  onClick={handleEnterButtonClick}>Skills</a></li>
         <li className={activeSection === 'contact' ? 'active' : ''}><a href="https://github.com/boydlakers15/certificates-jared">Certificates</a></li>
         {/* Add more creative elements to your menu, like icons or animations */}
         <div className="cursor"></div>
