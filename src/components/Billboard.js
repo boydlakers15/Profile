8c8d5c318840bcf709f4e17b5844b8ccadf3349c
Jared Boyd <jboyd115@icloud.com>
2024-01-05 14:34:24 +0100

    test

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -57,23 +57,22 @@ export function Model(props) {
         <group {...props} dispose={null}>
             <group>
                 <group {...props} dispose={null}>
-                <group name="Billboard_2">
-                    <group position={[1.122, 0.154, -0.288]}>
-                        <mesh name="Billboard_2_1002" geometry={nodes.Billboard_2_1002.geometry} material={materials['lambert3SG.002']} />
-                        {Html && (
-                            <OverlayItem
-                                rotation-y={degToRad(20)}
-                                position-x={-.2}
-                                position-z={-2.2}
-                                position-y={0.9}
-                                title={"Open to Discover"}
+                    <group name="Billboard_2">
+                        <group position={[1.122, 0.154, -0.288]}>
+                            <mesh name="Billboard_2_1002" geometry={nodes.Billboard_2_1002.geometry} material={materials['lambert3SG.002']} />
+                            {Html && (
+                                <OverlayItem
+                                    rotation-y={degToRad(20)}
+                                    position-x={-.2}
+                                    position-z={-2.2}
+                                    position-y={0.9}
+                                    title={"Open to Discover"}
 
-                                price={"129.99"}
-                                bgColor={"bg-yellow-500"}
-                                className={"transition delay-1000"}
-                            />
-                        )}
-                `` 
+                                    price={"129.99"}
+                                    bgColor={"bg-yellow-500"}
+                                    className={"transition delay-1000"}
+                                />
+                            )}
                             <mesh name="Billboard_2_1002" geometry={nodes.Billboard_2_1002.geometry} material={materials['lambert3SG.002']} />
                             <mesh name="Billboard_2_1002_1" geometry={nodes.Billboard_2_1002_1.geometry} material={materials['Material.006']} />
                             <mesh name="Billboard_2_1002_2" geometry={nodes.Billboard_2_1002_2.geometry} material={materials['Material.001']} />
@@ -82,12 +81,12 @@ export function Model(props) {
                             <mesh name="Billboard_2_1002_5" geometry={nodes.Billboard_2_1002_5.geometry} material={materials['Material.005']} />
                             <mesh name="Billboard_2_1002_6" geometry={nodes.Billboard_2_1002_6.geometry} material={materials['Material.003']} />
                             <mesh name="Billboard_2_1002_7" geometry={nodes.Billboard_2_1002_7.geometry} material={materials['Material.007']} />
-                    </group>``
-                    </group>
+                        </group>
                     </group>
                 </group>
             </group>
-       
+        </group>
+
 
 
     )
