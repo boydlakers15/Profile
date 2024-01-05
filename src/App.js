b64054f4291be271ef47b82dd0d6d558124f9a6e
Jared Boyd <jboyd115@icloud.com>
2024-01-05 14:02:21 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Profile from './components/Profile';
 import SkillsPage from './components/Skills';
