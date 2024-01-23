1c7ea44ed64a526ad9e545b221b857429879503c
Jared Boyd <jboyd115@icloud.com>
2024-01-23 14:01:06 +0100

    Changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,10 +6,10 @@ import {
   Text,
   useFont,
 } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree, extend  } from "@react-three/fiber";
 import { useAtom } from "jotai";
 import { useEffect, useRef } from "react";
-import { Color } from "three";
+import { Color, Raycaster, Vector2 } from "three";
 import { degToRad, lerp } from "three/src/math/MathUtils";
 import { Model } from "./Billboard";
 import { currentPageAtom } from "./UI";
@@ -18,6 +18,8 @@ import { currentPageAtom } from "./UI";
 const bloomColor = new Color("#fff");
 bloomColor.multiplyScalar(1.5);
 
+extend({ Raycaster });
+
 export const Experience = () => {
   const controls = useRef();
   const meshFitCameraHome = useRef();
@@ -25,13 +27,7 @@ export const Experience = () => {
   const textMaterial = useRef();
   const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
 
-  useFrame((_, delta) => {
-    textMaterial.current.opacity = lerp(
-      textMaterial.current.opacity,
-      currentPage === "home" || currentPage === "intro" ? 1 : 0,
-      delta * 1.5
-    );
-  });
+  
 
   const intro = async () => {
     controls.current.dolly(-22);
@@ -69,18 +65,7 @@ export const Experience = () => {
         <boxGeometry args={[12.9, 3, 3]} />
         <meshBasicMaterial color="orange" transparent opacity={0.9} />
       </mesh>
-      <Text
-        font={"fonts/Poppins-SemiBoldItalic.ttf"}
-        position-x={1.5}
-        position-y={-0.5}
-        position-z={3.2}
-        lineHeight={0.8}
-        textAlign="center"
-        rotation-y={degToRad(30)}
-        anchorY={"bottom"}
-        fontSize={0.7} // Adjust the font size as needed
-      >
-       WELCOME
+    
         <meshBasicMaterial
           color={bloomColor}
           toneMapped={false}
@@ -100,7 +85,17 @@ export const Experience = () => {
            
           </RenderTexture>
         </meshBasicMaterial>
-      </Text>
+
+        <meshBasicMaterial  >
+          <RenderTexture attach="map">
+            <color attach="background" args={['#fff']} />
+            <Environment preset="sunset" />
+            
+           
+           
+          </RenderTexture>
+        </meshBasicMaterial>
+   
 
      
       <group rotation-y={degToRad(-90)} position-x={4.7} position-y={-.4} position-z={2.1}>
@@ -116,13 +111,13 @@ export const Experience = () => {
           blur={[100, 100]}
           resolution={2048}
           mixBlur={1}
-          mixStrength={10}
+          mixStrength={5}
           roughness={1}
           depthScale={1.1}
           opacity={0.5}
           transparent
           minDepthThreshold={0.2}
-          maxDepthThreshold={1.4}
+          maxDepthThreshold={1.8}
           color="#333"
           metalness={0.5}
         />
