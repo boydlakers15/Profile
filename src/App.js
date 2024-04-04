6b6a7544d14c923bbc712eb7af1c7f9ceda95173
Jared Boyd <jboyd115@icloud.com>
2024-04-04 17:45:43 +0200

    Made changes to all

--- src/App.js
+++ src/App.js
@@ -10,24 +10,26 @@ import About from './components/About';
 import FooterPage from './components/FooterPage';
 import { Experience } from './components/Experience';
 import { UI } from './components/UI';
+
 // import ProjectText from './components/ProjectsText'
 // import { Canvas } from "@react-three/fiber";
 // import { Environment, OrbitControls } from '@react-three/drei';
 const App = () => {
   return (
     <>
+
       <Router>
         <Routes>
         
             {/* Use the "element" prop instead of "component" */}
-            <Route exact path="/Home" element={<LandingPage />} />
-            <Route exact path="/First" element={<Profile />} />
-            <Route exact path="/skills" element={<SkillsPage />} />
-            <Route exact path="/foot" element={<FooterPage />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/exper" element={<Experience />} />
-            <Route exact path="/ui" element={<UI />} />
-            <Route exact path="/nav" element={<Navbar />} />
+            <Route exact path="Profile/" element={<LandingPage />} />
+            <Route exact path="Profile/First" element={<Profile />} />
+            <Route exact path="Profile/skills" element={<SkillsPage />} />
+            <Route exact path="Profile/foot" element={<FooterPage />} />
+            <Route exact path="Profile/about" element={<About />} />
+            <Route exact path="Profile/exper" element={<Experience />} />
+            <Route exact path="Profile/ui" element={<UI />} />
+            <Route exact path="Profile/nav" element={<Navbar />} />
         </Routes>
       </Router>
       {/* <Canvas>
